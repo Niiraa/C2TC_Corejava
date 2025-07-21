73b477e65c66106c13c20d30aa3877dc4dd42a39
ETHNOTECH <ETHNOTECH@DESKTOP-P2VHN7O>
2025-07-21 12:25:02 +0530

    First java project

---
+++ C2TC_NIRANJANA/src/DAY1/FirstProgram.java
@@ -0,0 +1,11 @@
+package DAY1;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("welcome to java class");
+
+	}
+
+}
